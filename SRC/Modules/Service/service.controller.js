d6cd263795084284c4b804268516407a824ee245
Yasmeen radwan <engyradwan93@gmail.com>
2024-12-15 13:15:48 +0200

    adjust services

--- SRC/Modules/Service/service.controller.js
+++ SRC/Modules/Service/service.controller.js
@@ -169,7 +169,7 @@ export const deleteAllServices = async (req, res, next) => {
 
     res.status(200).json({
       status: "success",
-      message: "All services and associated resources deleted successfully",
+      message: "All services and resources deleted successfully",
     });
   } catch (error) {
     next(error); 
